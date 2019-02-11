efcecb990b6c3408e73b56e51fa97eded2d493a6
stomg7969 <stomg7969@gmail.com>
2019-02-11 14:02:31 -0500

    done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,6 +7,31 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
+  character = response_hash["results"].find { |person| person["name"].downcase == character_name}
+
+  character["films"].each do |movie|
+    a = RestClient.get(movie)
+    JSON.parse(a)
+  end
+
+
+  # films_array = []
+
+  # response_hash["results"].each do |person_hash|
+  #   if person_hash["name"] == character_name
+  #     # puts person_hash["films"]
+  #     films_array << person_hash["films"]
+  #   end
+  #   films_array[0]
+  # end
+
+  #
+  #
+  # films_array[0].each do |film_url|
+  #   this_film_api = RestClient.get(film_url)
+  #   this_film_api_hash = JSON.parse(this_film_api)
+  # end
+
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,6 +44,12 @@ def get_character_movies_from_api(character_name)
 end
 
 def print_movies(films)
+  # binding.pry
+  c = films.map do |movie|
+    a = RestClient.get(movie)
+    JSON.parse(a)["title"]
+  end
+  puts "Movies for this character are: #{c.join(", ")}"
   # some iteration magic and puts out the movies in a nice list
 end
 
